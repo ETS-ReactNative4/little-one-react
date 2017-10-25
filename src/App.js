d5d1eae51db8bc4792322715a68469dad46fa03b
robchamberspfc <robchamberspfc@gmail.com>
2017-10-25 21:43:20 +0100

    photos working

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 const photoCount=12
 const addPhotos=6
-const imgURL="https://api.github.com/repos/robchamberspfc/little-one/contents/content/"
+const imgURL="https://api.github.com/repos/robchamberspfc/little-one-images/contents/content"
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state={imageFolders:[], photoCount: photoCount, value: "" };
+    this.state={imageFolders:[], imageURLs:[], photoCount: photoCount};
     this.viewMorePhotos=this.viewMorePhotos.bind(this);
+    this.getPhotos=this.getPhotos.bind(this);
     }
   
   componentWillMount() {
@@ -26,7 +27,8 @@ class App extends Component {
             const imageFolders=[]
             json.map((data,index) => {
               imageFolders.push({title:data.name,url:data.url});
-          });  
+          });
+          console.log(imageFolders)
           this.setState({imageFolders:imageFolders})
         })
   }
@@ -37,24 +39,43 @@ class App extends Component {
   }
 
   getPhotos (option) {
-    console.log(option.target.value)
+
+          fetch(option.target.value, {
+            mode: 'cors'
+        })
+          .then(data => {return data.json()})
+          .then(json => {
+            //console.log(json)
+            const images=[]
+            json.map((data,index) => {
+              images.push({size:data.size,url:data.download_url});
+          });  
+          console.log(images)
+          this.setState({imageURLs:images})
+        })
+
+
   }
 
   render() {
     return (
         <div>
           <h1>Baby website</h1>
-
+          <p>
+            <select onChange={this.getPhotos} value={this.state.value}>
+              <option>Select a photo set</option>
+              {this.state.imageFolders.map((item,index) => {
+                return <option key={index} value={item.url}>{item.title}</option>})}
+              </select> 
+          </p>
+          <p>
+          {this.state.imageURLs.map((item,index) => {
+                return <img key={index} src={item.url} style={{width:200}}/> })}
+          </p>
           <h2>{this.state.photoCount}</h2>
           <button onClick={this.viewMorePhotos}>
           View more
-        </button>
-        <p>
-        <select onChange={this.getPhotos} value={this.state.value}>
-          {this.state.imageFolders.map((item,index) => {
-            return <option key={index} value={item.url}>{item.title}</option>})}
-          </select>
-        </p>
+          </button>
         </div>
     );
   }
